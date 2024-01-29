fd4c82ae0fc08d6996f9a8261e79d56f1e3173f9
Yuvraj171 <yuvrajkulwant2002@gmail.com>
2024-01-30 01:37:29 +0530

    open-ai-chats

--- dist/config/llama2-config.js
+++ dist/config/llama2-config.js
@@ -1,5 +1,9 @@
+//import { config } from "dotenv";
 import Replicate from "replicate";
-export const replicate = new Replicate({
-    auth: process.env.REPLICATE_API_TOKEN,
-});
+export const configurellama2AI = () => {
+    const replicate = new Replicate({
+        auth: process.env.REPLICATE_API_TOKEN,
+    });
+    return replicate;
+};
 //# sourceMappingURL=llama2-config.js.map
--- dist/controllers/chats-controllers.js
+++ dist/controllers/chats-controllers.js
@@ -1,17 +1,37 @@
 import Users from "../models/Users.js";
+import { OpenAIApi } from "openai";
+import { configureOpenAI } from "../config/openai-config.js";
 export const generateChatCompletioin = async (req, res, next) => {
     const { message } = req.body;
-    const user = await Users.findById(res.locals.jwData.id);
-    if (!user)
-        return res
-            .status(401)
-            .json({ message: "User not registered OR Token malfunctioned" });
-    // grab chats of user
-    const chats = user.chats.map(({ role, content }) => ({ role, content }));
-    //static array
-    chats.push({ content: message, role: "user" });
-    user.chats.push({ content: message, role: "user" });
-    // send all chats with new one to API
-    //get response
+    try {
+        const user = await Users.findById(res.locals.jwData.id);
+        if (!user)
+            return res
+                .status(401)
+                .json({ message: "User not registered OR Token malfunctioned" });
+        // grab chats of user
+        const chats = user.chats.map(({ role, content }) => ({
+            role,
+            content
+        }));
+        //static array
+        chats.push({ content: message, role: "user" });
+        user.chats.push({ content: message, role: "user" });
+        // send all chats with new one to API
+        const config = configureOpenAI();
+        const openai = new OpenAIApi(config);
+        //get latest response
+        const chatResponse = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: chats,
+        });
+        user.chats.push(chatResponse.data.choices[0].message);
+        await user.save();
+        return res.status(200).json({ chats: user.chats });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Something went wrong" });
+    }
 };
 //# sourceMappingURL=chats-controllers.js.map
